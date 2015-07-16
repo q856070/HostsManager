3f7295634c52d7a9fbe9a414859a817f4f14833e
q856070 <fjqiuq@gmail.com>
2015-07-16 18:13:57 +0800

    添加替换窗体

---
+++ HostsManager/FrmReplace.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace HostsManager {
+    partial class FrmReplace {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.btnReplaceAll = new System.Windows.Forms.Button();
+            this.txtSearchString = new System.Windows.Forms.TextBox();
+            this.lblFindStr = new System.Windows.Forms.Label();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.chkMatchTheCase = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtReplaceString = new System.Windows.Forms.TextBox();
+            this.btnReplace = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnReplaceAll
+            // 
+            this.btnReplaceAll.Location = new System.Drawing.Point(266, 112);
+            this.btnReplaceAll.Name = "btnReplaceAll";
+            this.btnReplaceAll.Size = new System.Drawing.Size(80, 25);
+            this.btnReplaceAll.TabIndex = 0;
+            this.btnReplaceAll.Text = "全部替换";
+            this.btnReplaceAll.UseVisualStyleBackColor = true;
+            // 
+            // txtSearchString
+            // 
+            this.txtSearchString.Location = new System.Drawing.Point(85, 29);
+            this.txtSearchString.Name = "txtSearchString";
+            this.txtSearchString.Size = new System.Drawing.Size(347, 21);
+            this.txtSearchString.TabIndex = 2;
+            // 
+            // lblFindStr
+            // 
+            this.lblFindStr.AutoSize = true;
+            this.lblFindStr.Location = new System.Drawing.Point(12, 32);
+            this.lblFindStr.Name = "lblFindStr";
+            this.lblFindStr.Size = new System.Drawing.Size(65, 12);
+            this.lblFindStr.TabIndex = 3;
+            this.lblFindStr.Text = "查询文本：";
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(352, 112);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(80, 25);
+            this.btnNext.TabIndex = 4;
+            this.btnNext.Text = "下一个";
+            this.btnNext.UseVisualStyleBackColor = true;
+            // 
+            // chkMatchTheCase
+            // 
+            this.chkMatchTheCase.AutoSize = true;
+            this.chkMatchTheCase.Location = new System.Drawing.Point(85, 117);
+            this.chkMatchTheCase.Name = "chkMatchTheCase";
+            this.chkMatchTheCase.Size = new System.Drawing.Size(84, 16);
+            this.chkMatchTheCase.TabIndex = 5;
+            this.chkMatchTheCase.Text = "匹配大小写";
+            this.chkMatchTheCase.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "替换文本：";
+            // 
+            // txtReplaceString
+            // 
+            this.txtReplaceString.Location = new System.Drawing.Point(85, 66);
+            this.txtReplaceString.Name = "txtReplaceString";
+            this.txtReplaceString.Size = new System.Drawing.Size(347, 21);
+            this.txtReplaceString.TabIndex = 6;
+            // 
+            // btnReplace
+            // 
+            this.btnReplace.Location = new System.Drawing.Point(180, 112);
+            this.btnReplace.Name = "btnReplace";
+            this.btnReplace.Size = new System.Drawing.Size(80, 25);
+            this.btnReplace.TabIndex = 8;
+            this.btnReplace.Text = "替换";
+            this.btnReplace.UseVisualStyleBackColor = true;
+            // 
+            // FrmReplace
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(444, 153);
+            this.Controls.Add(this.btnReplace);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtReplaceString);
+            this.Controls.Add(this.chkMatchTheCase);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.lblFindStr);
+            this.Controls.Add(this.txtSearchString);
+            this.Controls.Add(this.btnReplaceAll);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmReplace";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "替换";
+            this.Load += new System.EventHandler(this.FrmFind_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnReplaceAll;
+        private System.Windows.Forms.TextBox txtSearchString;
+        private System.Windows.Forms.Label lblFindStr;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.CheckBox chkMatchTheCase;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtReplaceString;
+        private System.Windows.Forms.Button btnReplace;
+    }
+}
--- HostsManager/FrmReplace.cs
+++ HostsManager/FrmReplace.cs
@@ -7,7 +7,7 @@ using System.Text;
 using System.Windows.Forms;
 
 namespace HostsManager {
-    public partial class FrmFind : Form {
+    public partial class FrmReplace : Form {
         /// <summary>
         /// 是否忽略大小写
         /// </summary>
@@ -17,24 +17,17 @@ namespace HostsManager {
         /// <summary>
         /// 选中文本
         /// </summary>
-        public FrmMain.dgSelect SelectText { get; set; }
+        public FrmMain.dgReplace ReplaceText { get; set; }
         /// <summary>
         /// 查找字符串
         /// </summary>
         public FrmMain.dgTextIndexOf TextIndexOf { get; set; }
-
-        public FrmFind() {
+        public FrmReplace() {
             InitializeComponent();
+
+            this.btnReplace.Click += btnReplace_Click;
             this.ShowInTaskbar = false;
             this.ShowIcon = false;
-            this.btnFind.Enabled = this.btnNext.Enabled = false;
-
-            this.btnFind.Click += btnFind_Click;
-            this.btnNext.Click += btnNext_Click;
-            this.chkMatchTheCase.Click += chkIgnoringTheCase_Click;
-            this.txtSearchString.KeyPress += txtSearchString_KeyPress;
-            this.txtSearchString.KeyUp += txtSearchString_KeyUp;
-                       
             this.CancelButton = new Button();
             (this.CancelButton as Button).Click += (sender, e) => { this.Close(); };
         }
@@ -45,10 +38,11 @@ namespace HostsManager {
                 this.Close();
             }
         }
+
         //回车响应搜索
         private void txtSearchString_KeyPress(object sender, KeyPressEventArgs e) {
             if (e.KeyChar == (char)Keys.Enter) {
-                btnFind_Click(sender, e);
+                btnReplace_Click(sender, e);
                 e.Handled = true;
                 return;
             }
@@ -56,7 +50,7 @@ namespace HostsManager {
         //启用/禁用查找
         private void txtSearchString_KeyUp(object sender, KeyEventArgs e) {
             string str = this.txtSearchString.Text.Trim();
-            this.btnFind.Enabled = str.Length > 0;
+            this.btnReplace.Enabled = str.Length > 0;
         }
         //匹配大小写
         private void chkIgnoringTheCase_Click(object sender, EventArgs e) {
@@ -64,12 +58,15 @@ namespace HostsManager {
         }
 
         //查找
-        private void btnFind_Click(object sender, EventArgs e) {
+        private void btnReplace_Click(object sender, EventArgs e) {
             if (this.txtSearchString.Text.Trim().Length == 0) {
                 MessageBox.Show("请输入查询字符串！");
                 return;
+            } else if (this.txtReplaceString.Text.Trim().Length == 0) {
+                MessageBox.Show("请输入替换字符串！");
+                return;
             }
-            bool exist = this.SearchString();
+            bool exist = this.ReplaceString();
             this.btnNext.Enabled = exist;
         }
         //下一个
@@ -77,27 +74,32 @@ namespace HostsManager {
             if (this.txtSearchString.Text.Trim().Length == 0) {
                 MessageBox.Show("请输入查询字符串！");
                 return;
+            } 
+            else if (this.txtReplaceString.Text.Trim().Length == 0) {
+                MessageBox.Show("请输入替换字符串！");
+                return;
             }
-            this.SearchString();
+            this.ReplaceString();
         }
 
         /// <summary>
         /// 搜索文本
         /// </summary>
-        private bool SearchString() {
-            string str = this.txtSearchString.Text.Trim();
+        private bool ReplaceString() {
+            string source = this.txtSearchString.Text.Trim();
+            string target = this.txtReplaceString.Text.Trim();
             //查找字符串是否存在
-            int idx = this.TextIndexOf(str, _index, this._matchTheCase);
+            int idx = this.TextIndexOf(source, _index, this._matchTheCase);
             if (idx > 0) {
-                this.SelectText(idx, str.Length);
+                this.ReplaceText(idx, source, target);
                 _index = idx + 1;
             } else {
                 if (_index > 0) {
                     MessageBox.Show("已经到达最底部！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
                     _index = 0;
-                    this.SelectText(0, 0);
+                    this.ReplaceText(0, source, "");
                 } else {
-                    MessageBox.Show("没有找到字符串！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    MessageBox.Show("没有可替换的字符串！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
             }
             return idx > 0;
